3ddc66474adfdde5442cb7098c7df69ef90e420e
ProVitSer <prokinvs@gmail.com>
2023-08-04 14:18:24 +0300

    feat: asterisk call and config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+import configuration from './config/config.provider';
+import { AsteriskModule } from './asterisk/asterisk.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ load: [configuration] }), AsteriskModule],
   controllers: [AppController],
   providers: [AppService],
+  exports: [ConfigModule],
 })
 export class AppModule {}
