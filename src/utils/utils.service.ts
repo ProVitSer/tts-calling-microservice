93c818eb39035da723066c816d0cd75d29de920a
ProVitSer <prokinvs@gmail.com>
2023-08-06 21:27:23 +0300

    generate unique id

--- src/utils/utils.service.ts
+++ src/utils/utils.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import * as uuid from 'uuid';
 
 @Injectable()
 export class UtilsService {
   static sleep(ms: number): Promise<any> {
     return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  static generateId(needUuid?: boolean): string {
+    if (!!needUuid) {
+      return uuid.v4();
+    } else {
+      return Date.now().toString(36) + Math.random().toString(36).substring(2);
+    }
   }
 }
