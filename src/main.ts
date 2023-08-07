d8093ebbffa965e739ebdbf1baface94c5414c02
ProVitSer <prokinvs@gmail.com>
2023-08-07 18:40:02 +0300

    feat: exceptionFactory

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,31 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import configuration from '@app/config/config.provider';
-import { ValidationPipe } from '@nestjs/common';
+import { HttpException, HttpStatus, ValidationError, ValidationPipe } from '@nestjs/common';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { UtilsService } from './utils/utils.service';
 
 async function bootstrap() {
   const config = new ConfigService(configuration());
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: (errors: ValidationError[]) => {
+        const messages = errors.map((error) => {
+          if (!!error.constraints) {
+            return {
+              field: error.property,
+              error: error.constraints,
+            };
+          } else if (Array.isArray(error.children) && error.children.length > 0) {
+            return UtilsService.formatError(error);
+          }
+        });
+        return new HttpException(messages.length > 1 ? messages : messages[0], HttpStatus.BAD_REQUEST);
+      },
+    }),
+  );
   app.useStaticAssets(join(__dirname, '..', 'public'));
   await app.listen(config.get('appPort'));
 }
