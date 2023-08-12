2939cc106f49add011f2b91a0c02080cd8ef8750
ProVitSer <prokinvs@gmail.com>
2023-08-12 23:44:55 +0300

    del log

--- src/calling/exceptions/calling-task-not-found.exception.ts
+++ src/calling/exceptions/calling-task-not-found.exception.ts
@@ -3,7 +3,6 @@ import { NotFoundException } from '@nestjs/common';
 class CallingTaskNotFoundException extends NotFoundException {
   constructor(applicationId: string, description?: string) {
     const message = description || `Задача с id ${applicationId} отсутствует`;
-    console.log(applicationId);
     super(message);
   }
 }
